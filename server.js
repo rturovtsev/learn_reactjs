5622b1af6ff8888c10a69b1c0175a305e9b22607
Sugrob69 <sugrob.boid@gmail.com>
2016-03-19 17:26:42 +0300

    init app

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use('/', express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+    console.log('Сервер запущен: http://localhost:' + app.get('port') + '/');
+});
